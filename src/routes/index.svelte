7e94a11cc4031f99d838b22080fb7ad4f6c96b90
Kyle Moore <20785882+YourFavoriteKyle@users.noreply.github.com>
2022-03-25 18:52:41 -0400

    Added client defined theme to tailwind.config.cjs
    Adjusted favicon in app.html
    Added client content to index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Frontend Mentor | Interactive rating component</title>
+
+	<!-- Add custom fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<div>
+	<!-- Rating state start -->
+
+	How did we do? Please let us know how we did with your support request. All feedback is
+	appreciated to help us improve our offering! 1 2 3 4 5 Submit
+
+	<!-- Rating state end -->
+
+	<!-- Thank you state start -->
+
+	You selected <!-- Add rating here --> out of 5 Thank you! We appreciate you taking the time to give
+	a rating. If you ever need more support, don’t hesitate to get in touch!
+
+	<!-- Thank you state end -->
+
+	<footer class="attribution">
+		Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+			>Frontend Mentor</a
+		>. Coded by <a href="#">Your Name Here</a>.
+	</footer>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	mode: 'jit',
 
 	theme: {
-		extend: {}
+		extend: {
+			screens: {
+				mobile: '375px',
+				desktop: '1440px'
+			},
+			colors: {
+				white: '#ffffff',
+				primary: '#fb7413',
+				'light-grey': '#959eac',
+				'medium-grey': '#7c8798',
+				'dark-blue': '#252d37',
+				'very-dark-blue': '#121417'
+			},
+			fontFamily: { sans: ['Overpass', ...defaultTheme.fontFamily.sans] }
+		}
 	},
 
 	plugins: []
